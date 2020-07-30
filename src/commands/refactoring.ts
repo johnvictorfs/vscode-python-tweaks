0baad78be64351a51d01d152149c5c19fc4fcdeb
John Victor <johnvictorfs@gmail.com>
2020-07-30 01:14:55 -0300

    Fixed message not appearing when no fstring conversions were made with command

--- src/commands/refactoring.ts
+++ src/commands/refactoring.ts
@@ -14,10 +14,16 @@ export function convertToFString() {
     const word = document.getText(selection)
 
     editor.edit(editBuilder => {
+      // Replace all valid strings into f-strings while keeping already valid f-strings the same
+      // Don't change strings without curly braces inside or only empty curly braces
       const pattern = /(f*)(["'])(\\?\3.*{+.+}+.*)(\2)/g
 
-      if (word.search(pattern) === -1) {
-        return vscode.window.showErrorMessage('No valid strings selected to convert')
+      const replaced = word.replace(pattern, 'f$2$3$4')
+
+      if (word !== replaced) {
+        vscode.window.showErrorMessage('No valid strings selected to convert')
+
+        return
       }
 
       editBuilder.replace(selection, word.replace(pattern, 'f$2$3$4'))
